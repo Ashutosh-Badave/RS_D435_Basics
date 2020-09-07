c271df18a6879ebd6361f8ce2fbf77701df4e662
Ashutosh-Badave <badave007@gmail.com>
2020-09-07 10:36:34 +0200

    Init: first commits

---
+++ src/RS_D435_launch.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ashutosh on 07.09.20.
+//
+
+#include <iostream>
+#include <numeric>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+#include <librealsense2/rs.hpp>
+
+int main()
+{
+    return 0;
+}
