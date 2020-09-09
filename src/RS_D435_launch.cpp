71e3a4bc3daaf039f722aff8e5f0953a9bbdb7d7
Ashutosh-Badave <badave007@gmail.com>
2020-09-09 11:40:17 +0200

    CU: modified to capture images with depth color and grayscale

--- src/RS_D435_launch.cpp
+++ src/RS_D435_launch.cpp
@@ -10,6 +10,8 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
 #include <librealsense2/rs.hpp>
+#include "../include/cv-helper.hpp" // from Intel OpenCV samples for Intel realsense camera
+
 
 using namespace cv;
 
@@ -29,12 +31,13 @@ int main(int argc, char * argv[]) try
 
     //resizeWindow(window_name,1200,800);
     int k = cv::waitKey(0); // Wait for a keystroke in the window
-    Mat visimage;
+    Mat depth_color,color_mat,depth_mat;
 
     while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
     {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
         // Try to get a frame of a depth image
+        auto color_frame = data.get_color_frame();
         auto depth_frame = data.get_depth_frame();
         rs2::frame depth = depth_frame.apply_filter(color_map);
 
@@ -43,22 +46,34 @@ int main(int argc, char * argv[]) try
         const int h = depth.as<rs2::video_frame>().get_height();
 
         // Create OpenCV matrix of size (w,h) from the colorized depth data
-        Mat image(Size(w, h), CV_8UC3, (void*)depth.get_data(), Mat::AUTO_STEP);
+        depth_color = Mat(Size(w, h), CV_8UC3, (void*)depth.get_data(), Mat::AUTO_STEP);
+
+        color_mat = frame_to_mat(color_frame);
+        depth_mat = depth_frame_to_meters(depth_frame);
 
         // Query the distance from the camera to the object in the center of the image
         // Query the distance from the camera to the object in the center of the image
         float dist_to_center = depth_frame.get_distance(w / 2, h / 2);
         // Update the window with new data
-        imshow(window_name, image);
+        imshow(window_name, depth_color);
+
+        //convert to grayscale
+        cvtColor(depth_color, depth_mat, COLOR_BGR2GRAY);
+
+        imshow("depth window",depth_mat);
+        imshow("color window",color_mat);
         // Print the distance
         std::cout << "The camera is facing an object " << dist_to_center << " meters away \n";
 
-        visimage = image;
 
     }
     if (k=='s')
     {
-        imwrite("../output/output.png",visimage);
+       // Mat dst;
+
+        imwrite("../output/depth_White.png",depth_mat);
+        imwrite("../output/color_img.png",color_mat);
+        imwrite("../output/depth_color.png",depth_color);
         std::cout << "Image is saved " << std::endl;
     }
 
