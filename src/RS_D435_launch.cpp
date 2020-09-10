77cc338586693b6e2f0cafb5ab5f4c834b03385e
Ashutosh-Badave <badave007@gmail.com>
2020-09-10 16:02:33 +0200

    CU: Pointcloud outputs from D435_PCL merged with OpenCV.

--- src/RS_D435_launch.cpp
+++ src/RS_D435_launch.cpp
@@ -1,5 +1,5 @@
 //
-// Base: Intel tutorial of Hello realsense.
+// Base: Intel tutorial of realsense with OpenCV and PCL.
 // Modification: Ashutosh Badave
 //
 
@@ -10,15 +10,84 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
 #include <librealsense2/rs.hpp>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/passthrough.h>
+
 #include "../include/cv-helper.hpp" // from Intel OpenCV samples for Intel realsense camera
 
 
 using namespace cv;
+using pcl_ptr = pcl::PointCloud<pcl::PointXYZ>::Ptr;
+
+struct Color
+{
+
+    float r, g, b;
+
+    Color(float setR, float setG, float setB)
+            : r(setR), g(setG), b(setB)
+    {}
+};
+
+pcl_ptr points_to_pcl(const rs2::points& points)
+{
+    pcl_ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    auto sp = points.get_profile().as<rs2::video_stream_profile>();
+    cloud->width = sp.width();
+    cloud->height = sp.height();
+    cloud->is_dense = false;
+    cloud->points.resize(points.size());
+    auto ptr = points.get_vertices();
+    for (auto& p : cloud->points)
+    {
+        p.x = ptr->x;
+        p.y = ptr->y;
+        p.z = ptr->z;
+        ptr++;
+    }
+
+    return cloud;
+}
+
+void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr& viewer, const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, std::string name, Color color = Color(1,1,1))
+{
+
+    viewer->addPointCloud<pcl::PointXYZ> (cloud, name);
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, name);
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, color.r, color.g, color.b, name);
+}
+
+
+pcl::visualization::PCLVisualizer::Ptr simpleVis ()
+{
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  //viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
+  //viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+  viewer->setCameraPosition(0, 0, 15, 0, 1, 0);
+  viewer->addCoordinateSystem (1.0);
+  return (viewer);
+}
+
 
 int main(int argc, char * argv[]) try
 {
+    // Create viewer
+    pcl::visualization::PCLVisualizer::Ptr viewer = simpleVis();
+
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
+
+    // Declare pointcloud object, for calculating pointclouds and texture mappings
+    rs2::pointcloud pc;
+    // We want the points object to be persistent so we can display the last cloud when a frame drops
+    rs2::points points;
 
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
@@ -30,16 +99,24 @@ int main(int argc, char * argv[]) try
     namedWindow(window_name, WINDOW_AUTOSIZE);
 
     //resizeWindow(window_name,1200,800);
-    int k = cv::waitKey(0); // Wait for a keystroke in the window
+    //int k = cv::waitKey(0); // Wait for a keystroke in the window
     Mat depth_color,color_mat,depth_mat;
 
-    while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
+    while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0 && !viewer->wasStopped ())
     {
+        viewer->removeAllPointClouds();
+        viewer->removeAllShapes();
+
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
         // Try to get a frame of a depth image
         auto color_frame = data.get_color_frame();
         auto depth_frame = data.get_depth_frame();
         rs2::frame depth = depth_frame.apply_filter(color_map);
+
+        // Generate the pointcloud and texture mappings
+        points = pc.calculate(depth_frame);
+
+        auto pcl_cloud = points_to_pcl(points);
 
         // Query frame size (width and height)
         const int w = depth.as<rs2::video_frame>().get_width();
@@ -65,9 +142,13 @@ int main(int argc, char * argv[]) try
         // Print the distance
         std::cout << "The camera is facing an object " << dist_to_center << " meters away \n";
 
+        renderPointCloud(viewer,pcl_cloud,"data");
+
+        viewer->spinOnce ();
+
 
     }
-    if (k=='s')
+    /*if (k=='s')
     {
        // Mat dst;
 
@@ -75,7 +156,7 @@ int main(int argc, char * argv[]) try
         imwrite("../output/color_img.png",color_mat);
         imwrite("../output/depth_color.png",depth_color);
         std::cout << "Image is saved " << std::endl;
-    }
+    }*/
 
     return EXIT_SUCCESS;
 }
