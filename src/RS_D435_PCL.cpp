68bf62607960e897d6886c47301124374acca703
Ashutosh-Badave <badave007@gmail.com>
2020-09-10 10:55:07 +0200

    Feature : Basics of PCL with D435.

---
+++ src/RS_D435_PCL.cpp
@@ -0,0 +1,17 @@
+//
+// Base: Intel tutorial of realsense with OpenCV and PCL.
+// Modification: Ashutosh Badave
+//
+
+#include <iostream>
+#include <numeric>
+#include <librealsense2/rs.hpp>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/passthrough.h>
+
+int main()
+{
+
+}
