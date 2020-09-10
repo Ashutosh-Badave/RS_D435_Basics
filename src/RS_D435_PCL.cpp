fb03872f20761cb8a1d91c02b26fdc793d533dfe
Ashutosh-Badave <badave007@gmail.com>
2020-09-10 11:01:52 +0200

    CU: Basic code taken from PCL tutorial of realsense.

--- src/RS_D435_PCL.cpp
+++ src/RS_D435_PCL.cpp
@@ -1,5 +1,5 @@
 //
-// Base: Intel tutorial of realsense with OpenCV and PCL.
+// Base: Intel tutorial of realsense with PCL.
 // Modification: Ashutosh Badave
 //
 
@@ -11,7 +11,52 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/passthrough.h>
 
+pcl_ptr points_to_pcl(const rs2::points& points)
+{
+    pcl_ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    auto sp = points.get_profile().as<rs2::video_stream_profile>();
+    cloud->width = sp.width();
+    cloud->height = sp.height();
+    cloud->is_dense = false;
+    cloud->points.resize(points.size());
+    auto ptr = points.get_vertices();
+    for (auto& p : cloud->points)
+    {
+        p.x = ptr->x;
+        p.y = ptr->y;
+        p.z = ptr->z;
+        ptr++;
+    }
+
+    return cloud;
+}
+
 int main()
 {
+    // Declare RealSense pipeline, encapsulating the actual device and sensors
+    rs2::pipeline pipe;
+    // Start streaming with default recommended configuration
+    pipe.start();
 
+    // Wait for the next set of frames from the camera
+    auto frames = pipe.wait_for_frames();
+
+    auto depth = frames.get_depth_frame();
+
+    // Generate the pointcloud and texture mappings
+    points = pc.calculate(depth);
+
+    auto pcl_points = points_to_pcl(points);
+
+    pcl_ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud(pcl_points);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(0.0, 1.0);
+    pass.filter(*cloud_filtered);
+
+    std::vector<pcl_ptr> layers;
+    layers.push_back(pcl_points);
+    layers.push_back(cloud_filtered);
 }
